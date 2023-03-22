49e17d6c317b049e7892854cab9fb54d8f9f468f
Andy Shen <andyashen12@gmail.com>
2023-03-21 17:26:59 -0700

    eda

---
+++ andy-eda/eda.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+
+load("../data/wave-1/DS1.rda")
+load("../data/wave-1/DS2.rda")
+load("../data/wave-1/DS3.rda")
+load("../data/wave-1/DS4.rda")
+
+ds1 <- da21600.0001
+ds2 <- da21600.0002
+ds3 <- da21600.0003
+ds4 <- da21600.0004
+
+View(ds1)
